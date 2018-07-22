a729d62ac04eb12a90e2fd04ceeb78b4a7c9c4b5
Chiahua <chiahua@Chiahuade-MacBook-Pro.local>
2018-07-22 16:02:55 -0400

    dispatcher added

---
+++ todoapp/src/constants/TodoConstants.js
@@ -0,0 +1,10 @@
+// CONSTANT
+// - used by STORE and ACTION
+
+export const CREATE_TODO = "CREATE_TODO";
+export const UPDATE_TODO = "UPDATE_TODO";
+export const UPDATE_TODO_COMPLETE = "UPDATE_TODO_COMPLETE";
+export const DELETE_TODO = "DELETE_TODO";
+
+
+    
--- todoapp/src/stores/TodoStore.js
+++ todoapp/src/stores/TodoStore.js
@@ -1,14 +1,22 @@
-import { EventEmitter } from 'events';
-
 // STORE
 // - STORE handles changes to the 'state' variables in react COMPONENTs
-// - STORE works as an event emitter, and tells the COMPONENTs to update their states after components rendered
+// - STORE works as an event emitter, and tells the COMPONENTs to update their states after the components rendered
+// - STORE registers its handles to the DISPATCHER, and allows the DISPATCHER to dispatch instructions when the 'state' variables in COMPONENTs needs to be updated
 
+// work as an event emitter
+import { EventEmitter } from 'events';
+
+// reference to constants and dispatcher
+import * as TodoConstant from '../constants/TodoConstants';
+import dispatcher from '../dispatchers/dispatcher';
 
 class TodoStore extends EventEmitter{
     constructor(){
         super()
         this.todolist = [{id : 0, content : 'testing react', complete : true}, {id : 1, content : 'create stores for holding states', complete : false}]
+    
+        // bind the action handle
+        this.handleActions = this.handleActions.bind(this)
     }
 
     // add new element to todolist state
@@ -53,8 +61,30 @@ class TodoStore extends EventEmitter{
     getAll(){
         return this.todolist
     }
+
+    // DISPATCHER uses this to instruct the STORE to update the 'state' variables in the COMPONENTs
+    handleActions(actions){
+        console.log(actions)
+
+        switch (actions.type){
+            case TodoConstant.CREATE_TODO:
+                this.createTodo(actions.id, actions.content, actions.complete)
+                break
+            case TodoConstant.UPDATE_TODO_COMPLETE:
+                this.updateTodo(actions.id, actions.complete)
+                break
+            case TodoConstant.UPDATE_TODO:
+                this.updateTodo(actions.id, actions.content, actions.complete)
+                break
+            case TodoConstant.DELETE_TODO:
+                this.deleteTodo(actions.id)
+        }
+    }
 }
 
 const todoStore = new TodoStore();
+
+// register dispatcher to handle all actions
+dispatcher.register(todoStore.handleActions)
 
 export default todoStore;
