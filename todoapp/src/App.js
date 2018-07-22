ab14303b517e1d7ca0c7700aff11fb428adcd261
Chiahua <chiahua@Chiahuade-MacBook-Pro.local>
2018-07-22 13:53:24 -0400

    setup components with state

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
+
 import EnterField from './components/EnterField';
 import TodoList from './components/TodoList';
 
 class App extends Component {
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      todolist : [{id : 0, content : 'testing react', complete : true}, {id : 1, content : 'create stores', complete : false}]
+    }
+  }
+
   render() {
+    const { todolist } = this.state;
+
     return (
       <div className="App">
-        <TodoList todolist={[{id : 0, content : 'testing react', complete : false}]}/>
+        <h1>Todos </h1>
+        <TodoList todolist={todolist}/>
         <EnterField />
       </div>
     );
